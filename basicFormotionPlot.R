e38483edd82ebf770a426eea40e8f0c2730799bf
Marc Kissel <marc.kissel@gmail.com>
2017-12-18 15:45:31 -0500

    Dec 18

--- basicFormotionPlot.R
+++ basicFormotionPlot.R
@@ -12,16 +12,13 @@ glimpse(war)
 
 library(ggrepel)
 p <- ggplot(war, aes(log(Military_size), log(Population), size = Military_size, color = stateabb, frame = year, label=stateabb)) +
-  geom_point() +  guides(color=FALSE) +
+  geom_point() + guides(color=FALSE) +
   geom_text_repel(aes(label=ifelse(Military_size > 449999,(stateabb),'')))
 
 
-gganimate(p, interval =.2, "outputMotion.gif")
+gganimate(p, interval =.2, "outputMotion2.gif")
 
 gganimate(p, interval = .2)
 
 
-p <- ggplot(war, aes(log(Military_size), log(Population), size = Military_size, color = stateabb, frame = year, label=stateabb)) +
-  geom_point() +  guides(color=FALSE) +
-  geom_text_repel(aes(label=ifelse(Military_size > 449999,(stateabb),''))) + theme(text = element_text(size = 12))
-p
+
